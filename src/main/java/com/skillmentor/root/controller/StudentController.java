3b6f3892e76b2c3e8b088097902fa82952ef303b
Ruwan <ruwanliyanagegalle1994@gmail.com>
2025-04-21 08:46:23 +0530

    Add controller, service for student

---
+++ src/main/java/com/skillmentor/root/controller/StudentController.java
@@ -0,0 +1,59 @@
+package com.skillmentor.root.controller;
+
+
+import com.skillmentor.root.dto.StudentDTO;
+import com.skillmentor.root.service.StudentService;
+import com.skillmentor.root.service.impl.StudentServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/v1/student")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping()
+    public ResponseEntity<StudentDTO> createStudent(@RequestBody StudentDTO studentDTO) {
+        studentService.createStudent(studentDTO);
+        return new ResponseEntity<>(studentDTO, HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<StudentDTO>> getAllStudents(@RequestParam(required = false) Integer age){
+        List<StudentDTO> studentDTOS = studentService.getAllStudents(age);
+        return new ResponseEntity<>(studentDTOS, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StudentDTO> findStudentById(@PathVariable Integer id){
+        StudentDTO student = studentService.getStudentById(id);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    @PutMapping()
+    public ResponseEntity<StudentDTO> updateStudent(@RequestBody StudentDTO studentDTO) {
+        StudentDTO student = studentService.updateStudentById(studentDTO);
+        return new ResponseEntity<>(studentDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<StudentDTO> deleteStudent(@PathVariable Integer id){
+        StudentDTO student = studentService.deleteStudentById(id);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/skillmentor/root/dto/StudentDTO.java
@@ -0,0 +1,82 @@
+package com.skillmentor.root.dto;
+
+public class StudentDTO {
+    private Integer studentId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private Integer age;
+
+    public StudentDTO(Integer studentId, String firstName, String lastName, String email, String phoneNumber, String address, Integer age) {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.age = age;
+    }
+
+    public StudentDTO() {
+
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getAge() {
+        return this.age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
+
